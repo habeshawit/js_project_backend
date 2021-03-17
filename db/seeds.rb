fb11cb4f1a69e0f490cca5a064b8a10f685aa9a0
habeshawit <shalomseleshi@gmail.com>
2021-03-17 19:17:11 -0400

    category model, migration and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+fantasy = Category.create(name: "Fantasy")
+adventure = Category.create(name: "Adventure")
+romance = Category.create(name: "Romance")
+mystery = Category.create(name: "Mystery")
+horror = Category.create(name: "Horror")
+thriller = Category.create(name: "Thriller")
+historical_fiction = Category.create(name: "Historical Fiction")
+sci_fi = Category.create(name: "Sci_fi")
+memoir = Category.create(name: "Memoir")
+educational = Category.create(name: "Educational")
+motivational = Category.create(name: "Motivational")
+children = Category.create(name: "Children's")
