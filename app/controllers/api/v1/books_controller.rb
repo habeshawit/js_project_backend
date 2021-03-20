b61bbc55434137b9910360c18b719c2d2d030ae2
habeshawit <shalomseleshi@gmail.com>
2021-03-20 18:46:25 -0400

    title syntax error fixed

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -14,14 +14,14 @@ class Api::V1::BooksController < ApplicationController
         if book.save
             render json: BookSerializer.new(book), status: :accepted
         else
-            render json: {errors: book.errors.full_message}, status: :unprocessible_entity
+            render json: {errors: book.errors.full_message}, status: :unprocessable_entity
         end
     end
 
     private
 
     def book_params
-        params.require(:book).permit(:ttle, :author, :description, :image_url, :price, :seller_info, :category_id)
+        params.require(:book).permit(:title, :author, :description, :image_url, :price, :seller_info, :category_id)
     end
 
 end
