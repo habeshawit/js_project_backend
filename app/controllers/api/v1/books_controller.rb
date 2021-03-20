53eb3a874f5fb560d20d39638c1c6acfc18f1658
habeshawit <shalomseleshi@gmail.com>
2021-03-19 22:57:19 -0400

    book controller index and create route added

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::BooksController < ApplicationController
+
+    def index
+        books = Book.all
+        render json: books
+    end
+
+    def create
+        book = Book.new(book_params)
+        if book.save
+            render json: book, status: :accepted
+        else
+            render json: {errors: book.errors.full_message}, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def book_params
+        params.require(:book).permit(:title, :author, :description, :image_url, :price, :seller_info, :category_id)
+    end
+
 end
