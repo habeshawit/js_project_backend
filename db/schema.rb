0bbca69b2a3a5596cce1c8aa545e8aa9180f9fb0
habeshawit <shalomseleshi@gmail.com>
2021-03-19 21:58:45 -0400

    category id added to book as reference

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_232221) do
+ActiveRecord::Schema.define(version: 2021_03_20_015140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,9 +22,10 @@ ActiveRecord::Schema.define(version: 2021_03_19_232221) do
     t.string "image_url"
     t.decimal "price"
     t.string "seller_info"
-    t.integer "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_books_on_category_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -33,4 +34,5 @@ ActiveRecord::Schema.define(version: 2021_03_19_232221) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "books", "categories"
 end
