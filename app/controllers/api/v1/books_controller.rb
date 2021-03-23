bbb2e7b07a040fda16ba65bcabc5d138ac107765
habeshawit <shalomseleshi@gmail.com>
2021-03-23 13:09:40 -0400

    book show added

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -18,6 +18,45 @@ class Api::V1::BooksController < ApplicationController
         end
     end
 
+    def show 
+        book = Book.find_by(id: params[:id])
+
+        if book 
+            render json: book
+        else 
+            render json: { message: 'Book not found.' }
+        end 
+    end 
+
+    def update
+        book = Book.find_by_id(params[:id])
+        book.title = params[:title]
+        book.author = params[:author]
+        book.price = params[:price]
+        book.description = params[:description]
+        book.seller_info = params[:seller_info]
+        book.image_url = params[:image_url]
+        if book.save
+            # flash[:success] = 'Book was successfully updated.'
+            render json: BookSerializer.new(book), status: :accepted
+        else
+            # flash[:error] = 'Something went wrong.'
+            render json: {errors: book.errors.full_message}, status: :unprocessable_entity
+        end
+        # book = Book.find_by(id: params[:id])
+        # byebug
+        # book.update(params)
+
+        # if book.save
+        #     byebug
+        #     render json: book
+            
+        # else 
+        #     render json: { message: 'book not updated.' }
+            
+        # end 
+    end
+
     private
 
     def book_params
